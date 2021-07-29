d9c9dc48b89004765de5dd8c76159b5ae77be224
njenga1011 <njenga.k11@gmail.com>
2021-07-29 13:12:02 +0300

    first commit

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#youtuber = "Njenga Karori"
+
+#print("subscribe to" + youtuber)
+#print("subscribe to {}".format (youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ") 
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ") 
+
+madlib = f"Programming is s0 {adj}! It makes me so excited evertime because i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
